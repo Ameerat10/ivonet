1dc07b12e59722301bbb6db0e8919c1bc4a25046
Ameera10 <a.ademuyiwa@alustudent.com>
2024-07-11 16:49:13 +0000

    "created views.py"

--- backend/ivonet/opportunities/views.py
+++ backend/ivonet/opportunities/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Feed, Dashboard, Application, Message, Opportunity, JobListing, Recommendation, Setting
+from .serializers import FeedSerializer, DashboardSerializer, ApplicationSerializer, MessageSerializer, OpportunitySerializer, JobListingSerializer, RecommendationSerializer, SettingSerializer
 
-# Create your views here.
+class FeedViewSet(viewsets.ModelViewSet):
+    queryset = Feed.objects.all()
+    serializer_class = FeedSerializer
+
+class DashboardViewSet(viewsets.ModelViewSet):
+    queryset = Dashboard.objects.all()
+    serializer_class = DashboardSerializer
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+class OpportunityViewSet(viewsets.ModelViewSet):
+    queryset = Opportunity.objects.all()
+    serializer_class = OpportunitySerializer
+
+class JobListingViewSet(viewsets.ModelViewSet):
+    queryset = JobListing.objects.all()
+    serializer_class = JobListingSerializer
+
+class RecommendationViewSet(viewsets.ModelViewSet):
+    queryset = Recommendation.objects.all()
+    serializer_class = RecommendationSerializer
+
+class SettingViewSet(viewsets.ModelViewSet):
+    queryset = Setting.objects.all()
+    serializer_class = SettingSerializer
+
