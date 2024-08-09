19bbde176aa1e35224ab07ac4190f44475135974
Ameerat10 <a.ademuyiwa@alustudent.com>
2024-08-09 00:47:47 +0000

    "mysqlclient"

--- backend/ivonet/ivonet/settings.py
+++ backend/ivonet/ivonet/settings.py
@@ -118,7 +118,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangodb',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'ameerapwd',
         'HOST': 'localhost',  # or the hostname where your MySQL server is running
         'PORT': '3306',      # or the port on which your MySQL server is listening
     }
