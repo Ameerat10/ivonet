adb092bb8dbb02fa4ae14f44fb58ff2f464e393c
ameerat10 <a.ademuyiwa@alustudent.com>
2024-08-09 15:25:57 +0000

    "altered the models"

--- backend/ivonet/authentication/models.py
+++ backend/ivonet/authentication/models.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# class CustomUser(AbstractUser):
+#     name = models.CharField(max_length=255)
+#     birth_date = models.DateField(null=True, blank=True)
+#     gender = models.CharField(max_length=10, null=True, blank=True)
+#     phone_number = models.CharField(max_length=20, null=True, blank=True)
+#     university = models.CharField(max_length=255, null=True, blank=True)
+#     university_year = models.CharField(max_length=20, null=True, blank=True)
+#     major = models.CharField(max_length=255, null=True, blank=True)
+#     areas_of_interest = models.TextField(null=True, blank=True)
+
+
 class CustomUser(AbstractUser):
     name = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
@@ -10,3 +21,5 @@ class CustomUser(AbstractUser):
     university_year = models.CharField(max_length=20, null=True, blank=True)
     major = models.CharField(max_length=255, null=True, blank=True)
     areas_of_interest = models.TextField(null=True, blank=True)
+    # Ensure username is unique and required
+    username = models.CharField(max_length=150, unique=True)
--- backend/ivonet/authentication/serializers.py
+++ backend/ivonet/authentication/serializers.py
@@ -15,23 +15,49 @@
 from rest_framework import serializers
 from .models import CustomUser
 
+# class CustomUserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = CustomUser
+#         fields = ('id', 'username', 'email', 'name', 'birth_date', 'gender', 'phone_number', 'university', 'university_year', 'major', 'areas_of_interest')
+#         extra_kwargs = {
+#             'password': {'write_only': True, 'required': False},
+#             'username': {'read_only': True},
+#         }
+
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.birth_date = validated_data.get('birth_date', instance.birth_date)
+#         instance.gender = validated_data.get('gender', instance.gender)
+#         instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+#         instance.university = validated_data.get('university', instance.university)
+#         instance.university_year = validated_data.get('university_year', instance.university_year)
+#         instance.major = validated_data.get('major', instance.major)
+#         instance.areas_of_interest = validated_data.get('areas_of_interest', instance.areas_of_interest)
+#         instance.save()
+#         return instance
+
+
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'name', 'birth_date', 'gender', 'phone_number', 'university', 'university_year', 'major', 'areas_of_interest')
+        fields = ('id', 'username', 'email', 'name', 'birth_date', 'gender', 'phone_number', 'university', 'university_year', 'major', 'areas_of_interest', 'password')
         extra_kwargs = {
-            'password': {'write_only': True, 'required': False},
-            'username': {'read_only': True},
+            'password': {'write_only': True, 'required': True},
+            'username': {'required': True},  # Ensure username is required and writable
         }
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.birth_date = validated_data.get('birth_date', instance.birth_date)
-        instance.gender = validated_data.get('gender', instance.gender)
-        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
-        instance.university = validated_data.get('university', instance.university)
-        instance.university_year = validated_data.get('university_year', instance.university_year)
-        instance.major = validated_data.get('major', instance.major)
-        instance.areas_of_interest = validated_data.get('areas_of_interest', instance.areas_of_interest)
-        instance.save()
-        return instance
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password'],
+            birth_date=validated_data.get('birth_date'),
+            gender=validated_data.get('gender'),
+            phone_number=validated_data.get('phone_number'),
+            university=validated_data.get('university'),
+            university_year=validated_data.get('university_year'),
+            major=validated_data.get('major'),
+            areas_of_interest=validated_data.get('areas_of_interest'),
+        )
+        return user
